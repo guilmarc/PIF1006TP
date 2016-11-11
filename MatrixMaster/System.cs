11f7d06a17b0f490e8b4c8f9470add0ea285afff
Lion001 <noundou.tchounga@gmail.com>
2016-11-11 12:19:47 -0400

    inverse

--- MatrixMaster/System.cs
+++ MatrixMaster/System.cs
@@ -42,7 +42,26 @@ namespace MatrixMaster
 		//retourner « null ».
 		public Matrix SolveByInversion()
 		{
-			return null;
+            if(this.Matrix1.Determinant == 0)
+            {
+                Console.WriteLine("Le determinant de la premiere matrice doit être différente de zéro.");
+                return null;
+            }
+            else
+            {
+                Matrix inverse1 = this.Matrix1.Inverse;
+                Matrix product1 = inverse1.Multiply(this.Matrix2);
+
+                var j = 1;
+
+                for(int i=1; i<= product1.GetLength(1); i++)
+                {
+                    var Xi = product1[i, j];
+                    Console.WriteLine();
+                }
+            }
+
+            return null;
 		}
 
 		//Matrice TrouverXParJacobi(double epsilon) : retourne une matrice X contenant les valeurs des inconnues en appliquant la méthode itérative de Jacobi;
